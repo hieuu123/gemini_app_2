d122e9cb65c1f93b3956b26ae9a2f59c311743ed
hieuu123 <tronghieuabc123zzz@gmail.com>
2025-05-19 18:41:21 +0700

    push vercel 2

--- api/firebase_config.py
+++ api/firebase_config.py
@@ -1,12 +1,11 @@
+# api/firebase_config.py
 import os, json
 from firebase_admin import credentials, initialize_app, firestore
 
-# Nạp JSON key từ ENV VAR
+# Load service account JSON (được đưa vào ENV variable FIREBASE_SA_JSON)
 sa_json = os.environ["FIREBASE_SA_JSON"]
 cred    = credentials.Certificate(json.loads(sa_json))
+initialize_app(cred, {"projectId": os.environ["FIREBASE_PROJECT_ID"]})
 
-# Khởi app với projectId từ ENV
-initialize_app(cred, { "projectId": os.environ["FIREBASE_PROJECT_ID"] })
-
-# Xuất client Firestore toàn cục
+# Export Firestore client
 db = firestore.client()
--- api/index.py
+++ api/index.py
@@ -1,9 +1,9 @@
-from flask import Flask, send_from_directory
-from firebase_config import db
-from routes.main import main_bp
-from routes.search import search_bp
 import os
-# … import các blueprint khác
+from flask import Flask, send_from_directory
+from routes.search import search_bp
+from routes.main   import main_bp
+# …
+from .firebase_config import db        # <-- import tương đối
 
 app = Flask(__name__, static_folder="../static", static_url_path="/")
 app.register_blueprint(main_bp)
@@ -13,7 +13,7 @@ app.register_blueprint(search_bp)
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def static_proxy(path):
-    full = app.static_folder + "/" + path
+    full = os.path.join(app.static_folder, path)
     if path and os.path.exists(full):
         return send_from_directory(app.static_folder, path)
     return send_from_directory(app.static_folder, "index.html")
