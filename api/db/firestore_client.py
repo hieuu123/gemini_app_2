c95161d03d23c7753b24cb17d545ad05cc031e8e
hieuu123 <tronghieuabc123zzz@gmail.com>
2025-05-19 21:56:04 +0700

    push vercel 3

--- api/db/firestore_client.py
+++ api/db/firestore_client.py
@@ -1,14 +1,19 @@
 # db/firestore_client.py
-import os
-import firebase_admin
-from firebase_admin import credentials, firestore
-import config
+import os, json
+from firebase_admin import credentials, initialize_app, firestore
 
-# Chỉ init một lần
-if not firebase_admin._apps:
-    cred = credentials.Certificate(config.FIREBASE_CREDENTIALS_PATH)
-    firebase_admin.initialize_app(cred, {
-        "projectId": config.FIREBASE_PROJECT_ID,
-    })
+# nếu có JSON string ở env thì parse, còn không fallback về path file
+sa_json = os.getenv("FIREBASE_SA_JSON")
+project_id = os.getenv("FIREBASE_PROJECT_ID")
 
+if sa_json:
+    cert_dict = json.loads(sa_json)
+    cred = credentials.Certificate(cert_dict)
+else:
+    path = os.getenv("FIREBASE_CREDENTIALS_PATH")
+    if not path:
+        raise RuntimeError("Bạn phải set FIREBASE_SA_JSON hoặc FIREBASE_CREDENTIALS_PATH")
+    cred = credentials.Certificate(path)
+
+initialize_app(cred, {"projectId": project_id})
 db = firestore.client()
